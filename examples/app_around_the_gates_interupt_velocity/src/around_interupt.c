cd07a1549034e0bc524dc59b38955a7cdf9e8ef1
alopez6991 <alopez6991@gmail.com>
2025-11-03 22:13:48 +0100

    it worked 1.5/2 times!

--- examples/app_around_the_gates_interupt_velocity/src/around_interupt.c
+++ examples/app_around_the_gates_interupt_velocity/src/around_interupt.c
@@ -57,10 +57,10 @@
 #define TAKEOFF_VEL_MPS           0.30f
 
 // Segment distances
-#define SEG2_POS_Y_M              1.0f        // +Y
-#define SEG3_POS_X_M              2.6f        // +X
+#define SEG2_POS_Y_M              0.75f        // +Y
+#define SEG3_POS_X_M              5.6f        // +X
 #define SEG4_NEG_Y_M              7.1f        // -Y
-#define SEG5_NEG_X_M              2.6f        // -X (final leg)
+#define SEG5_NEG_X_M              5.6f        // -X (final leg)
 
 // Multiranger avoidance
 #define AVOID_MIN_DIST_M          0.20f       // start pushing if closer than this
