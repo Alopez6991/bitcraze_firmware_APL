1081f0b903e8c500cfd1359139fa04a3846427a0
alopez6991 <alopez6991@gmail.com>
2025-11-04 17:12:37 +0100

    adding second drone

--- examples/app_around_the_gates_interupt_velocity/src/around_interupt.c
+++ examples/app_around_the_gates_interupt_velocity/src/around_interupt.c
@@ -60,7 +60,7 @@
 #define SEG2_POS_Y_M              0.75f        // +Y
 #define SEG3_POS_X_M              5.2f        // +X
 #define SEG4_NEG_Y_M              7.1f        // -Y
-#define SEG5_NEG_X_M              5.1f        // -X (final leg)
+#define SEG5_NEG_X_M              5.2f        // -X (final leg)
 
 // Multiranger avoidance
 #define AVOID_MIN_DIST_M          0.20f       // start pushing if closer than this
@@ -68,7 +68,7 @@
 
 // Down-range spike guard (units mm) with near-target gating at 0.2 m
 #define STEP_THRESH_MM            90.0f      // step change threshold (mm)
-#define RATE_THRESH_MM_PER_S      300.0f      // rate threshold (mm/s)
+#define RATE_THRESH_MM_PER_S      200.0f      // rate threshold (mm/s)
 #define ENABLE_SPIKE_NEAR_DIST_M  0.15f       // only arm spike stop when this close to segment target
 
 // Post-final-right (here final -X) creep and wait
